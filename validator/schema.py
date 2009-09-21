6bea7ce974320ef94342ab2ed75eb4a97d81d1f7
Anton Bobrov <bobrov@bober.localdomain>
2009-09-21 21:26:46 +1100

    initial commit

---
+++ validator/schema.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class SchemaNotFoundException(Exception):
+    pass
+
+class SchemaCatalog(object):
+    def __init__(self):
+        self.data = {}
+
+    def addFile(namespace, filename):
+        self.data[namespace]
+        
+
+class SchemaValidator(object):
+    pass
+    
