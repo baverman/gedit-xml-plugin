351523b4592f22a96ca1abf35faf91d6a8f4c9ca
Anton Bobrov <bobrov@bober.localdomain>
2009-09-21 22:18:43 +1100

    implemented xml validator against multiple schemas

--- validator/schema.py
+++ validator/schema.py
@@ -1,16 +1,45 @@
 # -*- coding: utf-8 -*-
 
-class SchemaNotFoundException(Exception):
-    pass
+from lxml import etree
 
-class SchemaCatalog(object):
-    def __init__(self):
-        self.data = {}
 
-    def addFile(namespace, filename):
-        self.data[namespace]
-        
+class SchemaNotFoundException(Exception): pass
+
 
 class SchemaValidator(object):
-    pass
+    def __init__(self, root, catalog):
+        self.catalog = catalog
+        self.root = root
+        self.schema = self.get_schema(root)
+
+
+    def get_xsd_filename_for_ns(self, ns):
+        try:
+            return self.catalog[ns]
+        except KeyError:
+            raise SchemaNotFoundException("Can't find xsd for [%s] namespace" % ns)
+        
     
+    def get_schema(self, root):
+        merged_schema = \
+            '<?xml version="1.0"?>' \
+            '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" ' \
+            'targetNamespace="http://global.namespace" ' \
+            'xmlns="http://global.namespace" ' \
+            'elementFormDefault="qualified">' \
+        
+        for ns in root.nsmap.values():
+            filename = self.get_xsd_filename_for_ns(ns)
+            merged_schema += '<xs:import schemaLocation="file://%s" />' % filename
+            
+        merged_schema += '</xs:schema>'
+        
+        return etree.XMLSchema(etree.XML(merged_schema))
+        
+    def validate(self):
+        return self.schema.validate(self.root)
+        
+    def get_error_log(self):
+        return self.schema.error_log
+        
+    error_log = property(get_error_log)
